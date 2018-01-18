f1f9e129db3c015c3f87000942638bbced461e72
Rob Prouse <rob@prouse.org>
2018-01-18 18:10:33 -0500

    Initial project structure

---
+++ SudokuSharp.Test/SudokuBoardTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SudokuSharp.Test
+{
+    [TestFixture]
+    public class SudokuBoardTest
+    {
+        SudokuBoard _board;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _board = new SudokuBoard();
+        }
+
+        [Test]
+        public void CanCreate()
+        {
+            Assert.That(_board, Is.Not.Null);
+        }
+
+        [Test]
+        public void BoardIsInitializedToUnset()
+        {
+            for (ushort row = 0; row < 9; row++)
+            for (ushort column = 0; column < 9; column++)
+            {
+                Assert.That(_board.Cell(row, column), Is.EqualTo(0));
+            }
+        }
+    }
+}
---
+++ SudokuSharp/SudokuBoard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SudokuSharp
+{
+    public class SudokuBoard
+    {
+        // 0 indicates not filled in
+        public ushort[,] _board = new ushort[9, 9];
+
+        public ushort Cell(ushort row, ushort column)
+        {
+            if (row > 8) throw new ArgumentOutOfRangeException(nameof(row));
+            if (column > 8) throw new ArgumentOutOfRangeException(nameof(column));
+            return _board[row, column];
+        }
+    }
+}
